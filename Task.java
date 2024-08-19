0b41d0780eba23f3fd6ec409c4de70ee0ed5eb74
PhuocLoc <lephuocloc1729@gmail.com>
2024-08-19 14:37:46 +0700

    add createdAt and updatedAt attributes to the Task class

--- Task.java
+++ Task.java
@@ -1,13 +1,19 @@
+import java.time.LocalDateTime;
+
 public class Task {
     private static int lastId = 0;  // Static variable to keep track of the last ID assigned
     private int id;
     private String description;
     private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Task(String description){
         this.id = ++lastId;
         this.description = description;
         this.status = Status.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
     public int getId(){
@@ -16,14 +22,17 @@ public class Task {
 
     public void markInProgress(){
         this.status = Status.IN_PROGRESS;
+        this.updatedAt = LocalDateTime.now();
     }
 
     public void markDone(){
         this.status = Status.DONE;
+        this.updatedAt = LocalDateTime.now();
     }
 
     public void updateDescription(String description){
         this.description = description;
+        this.updatedAt = LocalDateTime.now();
     }
 
     public String toJson(){
